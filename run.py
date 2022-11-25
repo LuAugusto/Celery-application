dabeac7ccd1bba11e799f15767dcc8425520c107
luciano <luciano.augusto@smarttbotter.com>
2022-11-25 09:20:52 -0300

    feat: updated readme

--- run.py
+++ run.py
@@ -5,12 +5,14 @@ from src.modules.locations.use_cases.get_when_sunrises_earlier.get_when_sunrises
 from src.modules.locations.use_cases.get_locations_with_less_wind.get_locations_with_less_wind_controller import get_locations_with_less_wind_controller
 from src.modules.locations.use_cases.get_average_temperature.get_average_temperature_controller import get_average_temperature_controller
 
-@server.app.route('/')
+@server.app.route('/locations')
 def get_locations():
     return get_locations_controller.execute()
 
 @server.app.route('/locations/sunrises/earlier/<day>')
 def get_when_sunrises_earlier(day):
+    if not(day.isdigit()):
+        return Response(status=404, response='Day is a number')
     return get_when_sunrises_earlier_controller.execute(day)
 
 @server.app.route('/locations/wind')
@@ -19,6 +21,8 @@ def get_locations_with_less_wind():
 
 @server.app.route('/locations/average/temperature/<city>')
 def get_averag_temperature(city):
+    if city.isdigit():
+        return Response(status=404, response='City is a string')
     return get_average_temperature_controller.execute(city)
 
 
